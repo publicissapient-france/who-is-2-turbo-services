ebb2adcca02866d793b7e8c96939f14573026a38
Remi Georges <remi.georges.ext@paylibservices.fr>
2021-03-01 17:58:02 +0100

    read from firestore v.0

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.status(400);
+  response.send({nom: "toto"});
+});
+
+export const listPersons = functions.https.onRequest(
+    async (request, response) => {
+      functions.logger.info("List all persons", {structuredData: true});
+      const readResult = await admin.firestore()
+          .collection("persons")
+          .listDocuments();
+      const json = await Promise.all(
+          readResult.map(async (it) => {
+            const doc = await it.get();
+            return {
+              firstName: doc.get("firstName"),
+              lastName: doc.get("lastName"),
+            } as Person;
+          }));
+
+      response.status(200);
+      response.send(json);
+    });
+
+type Person = {
+  firstName: string;
+  lastName: string;
+}
