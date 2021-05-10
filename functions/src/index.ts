5247d6c9b9ee1c959fa408d376f819e8f69d8d38
cdreyfus <cdreyfus@xebia.fr>
2021-05-10 22:58:30 +0200

    add firebase authentication

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { WhoisTurboModule } from './whois-turbo.module';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { Express } from 'express-serve-static-core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -21,7 +21,7 @@ const createNestServer = async (expressInstance: Express) => {
       'https://whois.publicissapient.fr',
     ],
   });
-
+  app.use(validateFirebaseIdToken);
   const config = new DocumentBuilder()
     .setTitle('Who Is Turbo V2')
     .setDescription('The Who Is Turbo V2 API description')
@@ -41,3 +41,40 @@ createNestServer(server)
   .catch((err) => console.error('Nest broken', err));
 
 export const api = functions.region('europe-west1').https.onRequest(server);
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = async (req: Request, res: Response, next: Function) => {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.error(
+      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>',
+    );
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    // No token
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    req.body.email = decodedIdToken.email;
+    next();
+    return;
+  } catch (error) {
+    functions.logger.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
