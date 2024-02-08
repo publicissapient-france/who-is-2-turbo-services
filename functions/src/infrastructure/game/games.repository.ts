c3c59f5345abb1a7ca7bd7275e32e829613a3e88
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-02-08 14:16:56 +0100

    Fix bug

--- functions/src/infrastructure/game/games.repository.ts
+++ functions/src/infrastructure/game/games.repository.ts
@@ -19,13 +19,6 @@ export class GamesRepository implements GameRepositorySpi {
   async fetchSeries(id: string): Promise<SeriesGameSession> {
     const updatedTime = (await this.gamesCollection.doc(id).update({ FINISHED: true })).writeTime;
     const documentSnapshot = await this.gamesCollection.doc(id).get();
-    console.log(documentSnapshot);
-    console.log('TIME');
-    console.log(updatedTime);
-    console.log(documentSnapshot.createTime?.toDate()?.getTime());
-    console.log(
-      updatedTime.toDate().getTime() - (documentSnapshot.createTime?.toDate()?.getTime() ?? 0),
-    );
     if (!documentSnapshot.exists) {
       console.log('No such document!', id);
     }
